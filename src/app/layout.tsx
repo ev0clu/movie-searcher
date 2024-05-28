94d117cacc5698e9a0b97b79296fc41748df97e8
ev0clu <ev0clu@gmail.com>
2024-05-28 11:14:30 +0200

    Root layout is modified, style and metadata description are updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Movie Searcher App',
-  description: 'App to search and get details of movies'
+  description: 'Let user to search and get details about movies'
 };
 
 export default function RootLayout({
@@ -17,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={
-          `flex min-h-screen flex-col gap-3 bg-background antialiased ${inter.className}`
-        }
+        className={`flex min-h-screen flex-col font-sans antialiased ${inter.className}`}
       >
+        <Header />
         <main className="flex flex-1 flex-col">{children}</main>
+        <Footer />
       </body>
     </html>
   );
