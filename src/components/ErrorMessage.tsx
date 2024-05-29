c06896c630df7b5e422c8039017b17de217c24d5
ev0clu <ev0clu@gmail.com>
2024-05-29 15:48:37 +0200

    ErrorMessage component is updated

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,3 +1,4 @@
+import { TriangleAlert } from 'lucide-react';
 import { ReactNode } from 'react';
 
 type ErrorMessageProps = {
@@ -9,9 +10,10 @@ const ErrorMessage = ({ children, className }: ErrorMessageProps) => {
   return (
     <div
       className={`
-          flex flex-1 flex-col items-center justify-center
+          mt-1 flex flex-1 flex-row items-center justify-center gap-2 text-red-700
           ${className}`}
     >
+      <TriangleAlert className="h-4 w-4" />
       {children}
     </div>
   );
