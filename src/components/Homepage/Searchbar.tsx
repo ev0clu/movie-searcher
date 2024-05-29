16344ccd8b9dd2c13d30236ca46d8f01123a0ea0
ev0clu <ev0clu@gmail.com>
2024-05-29 15:49:02 +0200

    Error handling is added to the Searchbar component

--- src/components/Homepage/Searchbar.tsx
+++ src/components/Homepage/Searchbar.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { Search } from 'lucide-react';
 import ContainerWrapper from '../ContainerWrapper';
+import ErrorMessage from '../ErrorMessage';
 
 type SearchbarProps = {
   searchData: (title: string) => void;
@@ -10,23 +11,35 @@ type SearchbarProps = {
 
 const Searchbar = ({ searchData }: SearchbarProps) => {
   const [inputValue, setInputValue] = useState('');
+  const [isError, setIsError] = useState(false);
 
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
+    if (inputValue !== '') {
+      setIsError(false);
+    }
     setInputValue(e.target.value);
   };
 
-  const handleSearch = (e: React.MouseEvent<HTMLElement>) => {
+  const handleSearchButton = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault;
-    searchData(inputValue);
-    setInputValue('');
+    searchHandler();
   };
 
   const handleInputKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>
   ) => {
     if (e.key === 'Enter' && inputValue !== '') {
+      searchHandler();
+    }
+  };
+
+  const searchHandler = () => {
+    if (inputValue === '') {
+      setIsError(true);
+    } else {
+      setIsError(false);
       searchData(inputValue);
       setInputValue('');
     }
@@ -34,21 +47,28 @@ const Searchbar = ({ searchData }: SearchbarProps) => {
 
   return (
     <ContainerWrapper className="h-32 flex-row items-center justify-center rounded-lg bg-yellow-500">
-      <div className="relative mx-5 flex w-96 items-center justify-center">
-        <input
-          className="h-10 w-full max-w-md rounded-lg border-none px-3 py-2 text-sm font-medium outline-none ring-0 ring-neutral-600 focus:ring-2"
-          type="text"
-          placeholder="Search for a movie"
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyDown={handleInputKeyDown}
-        />
-        <button
-          className="absolute right-0 flex h-9 w-9 items-center justify-center rounded-lg hover:bg-neutral-100"
-          onClick={handleSearch}
-        >
-          <Search className="h-6 w-6" />
-        </button>
+      <div className="relative mx-5">
+        <div className="relative flex w-96 items-center justify-center">
+          <input
+            className="h-10 w-full max-w-md rounded-lg border-none px-3 py-2 text-sm font-medium outline-none ring-0 ring-neutral-600 focus:ring-2"
+            type="text"
+            placeholder="Search for a movie"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyDown={handleInputKeyDown}
+          />
+          <button
+            className="absolute right-0 flex h-9 w-9 items-center justify-center rounded-lg hover:bg-neutral-100"
+            onClick={handleSearchButton}
+          >
+            <Search className="h-6 w-6" />
+          </button>
+        </div>
+        {isError && (
+          <ErrorMessage className="absolute">
+            Input field is required
+          </ErrorMessage>
+        )}
       </div>
     </ContainerWrapper>
   );
