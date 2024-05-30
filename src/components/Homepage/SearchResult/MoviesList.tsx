169892f7ba78035453cbfcced2944561a27a755a
ev0clu <ev0clu@gmail.com>
2024-05-30 13:46:18 +0200

    Style is updated

--- src/components/Homepage/SearchResult/MoviesList.tsx
+++ src/components/Homepage/SearchResult/MoviesList.tsx
@@ -1,5 +1,5 @@
 import { TMoviesResultsWithGenreNames } from '@/types/moviesResultsWithGenreNames.type';
-import { Star } from 'lucide-react';
+import { SquareArrowOutUpRight, Star } from 'lucide-react';
 import Link from 'next/link';
 
 type MoviesListProps = {
@@ -17,18 +17,21 @@ const MoviesList = ({ moviesWithGenreNames }: MoviesListProps) => {
         moviesWithGenreNames?.map((movie) => {
           return (
             <div key={movie.id}>
-              <li className="py-4 hover:cursor-pointer hover:bg-yellow-500 hover:bg-opacity-15">
-                <div className="mx-5 flex flex-row items-center justify-between gap-2">
-                  <Link
-                    href="/details"
-                    className="w-20 overflow-x-auto md:w-36 lg:w-48"
-                  >
-                    {movie.title}
-                  </Link>
-                  <div className="w-20 overflow-x-auto md:w-36 lg:w-48">
+              <li className="py-4 hover:bg-yellow-500 hover:bg-opacity-15">
+                <div className="mx-5 flex flex-row items-center justify-between gap-5">
+                  <div className="w-20 overflow-x-auto hover:cursor-pointer sm:w-32 md:w-44 lg:w-56">
+                    <Link
+                      href={`/movie/${movie.id}`}
+                      className="flex w-max flex-row font-semibold text-neutral-700"
+                    >
+                      <SquareArrowOutUpRight className="h-3 w-3" />
+                      {movie.title}
+                    </Link>
+                  </div>
+                  <div className="w-20 overflow-x-auto sm:w-32 md:w-44 lg:w-56">
                     {movie.genreNames.join(', ')}
                   </div>
-                  <div className="flex w-20 items-center justify-end gap-2 text-end md:w-36 lg:w-48">
+                  <div className="flex w-20 items-center justify-end gap-2 text-end sm:w-32 md:w-44 lg:w-56">
                     <Star
                       className="h-5 w-5"
                       fill="#eab308"
