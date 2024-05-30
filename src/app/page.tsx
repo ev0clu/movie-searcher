420838c474d34efb01a879446d2834ac83f9c969
ev0clu <ev0clu@gmail.com>
2024-05-30 10:58:08 +0200

    Page handling  is added to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,30 @@ import SearchResult from '@/components/Homepage/SearchResult';
 
 export default function Home() {
   const [title, setTitle] = useState('');
+  const [page, setPage] = useState(1);
 
   const searchData = (input: string) => {
     setTitle(input);
+    setPage(1);
+  };
+
+  const previousPage = () => {
+    setPage((prevState) => Math.max(prevState - 1, 0));
+  };
+
+  const nextPage = () => {
+    setPage((prevState) => prevState + 1);
   };
 
   return (
     <div className="flex w-full flex-1 flex-col items-center gap-2 px-10 pb-10 md:px-24 md:pb-24">
       <Searchbar searchData={searchData} />
-      {!!title && <SearchResult title={title} />}
+      <SearchResult
+        title={title}
+        page={page}
+        previousPage={previousPage}
+        nextPage={nextPage}
+      />
     </div>
   );
 }
